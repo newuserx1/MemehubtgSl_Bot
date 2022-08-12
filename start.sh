781f2a0904fb0b819abec55c82f6feca4d6b3814
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-12 15:45:15 +0530

    `MemehubtgSl_Bot`

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/TeamAlphaTg/MemehubtgSl_Bot.git /MemehubtgSl_Bot
+cd /MemehubtgSl_Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
